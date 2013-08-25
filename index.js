cb0ace70867db965d405de89bd17eba232bcf175
Dominic Tarr <dominic.tarr@gmail.com>
2013-08-25 13:29:38 +0100

    turn into pager

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 var spawn = require('child_process').spawn;
 
-module.exports = function (file, opts, cb) {
+module.exports = function (opts, cb) {
     if (typeof opts === 'function') {
         cb = opts;
         opts = {};
     }
     if (!opts) opts = {};
     
-    var ed = /^win/.test(process.platform) ? 'notepad' : 'vim';
-    var editor = opts.editor || process.env.VISUAL || process.env.EDITOR || ed;
+    var pager = opts.pager 
+      || process.env.PAGER 
+      || 'more'
     
     setRaw(true);
-    var ps = spawn(editor, [ file ], { customFds : [ 0, 1, 2 ] });
+    var ps = spawn(pager, [], { customFds : [ null, 1, 2 ] });
     
     ps.on('exit', function (code, sig) {
         setRaw(false);
         process.stdin.pause();
         if (typeof cb === 'function') cb(code, sig)
     });
+
+    return ps.stdin
 };
 
 var tty = require('tty');
