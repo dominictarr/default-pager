04ed75ebec6e7f9fae3005da75033f37beecb85a
Dominic Tarr <dominic.tarr@gmail.com>
2013-08-25 13:40:40 +0100

    tidy

--- example.js
+++ example.js
@@ -3,6 +3,6 @@ var fs = require('fs')
 var pager = require('./')
 
 fs.createReadStream(__dirname + '/README.markdown')
-.pipe(pager(function () {
-  console.log("DONE")
-}))
+  .pipe(pager(function () {
+    console.log("DONE")
+  }))
